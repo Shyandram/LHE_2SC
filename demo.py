73ee56769c9183c7a13f29d6269ff1a99abe0add
shyandram <shyangen104@gmail.com>
2023-02-10 23:49:26 +0800

    except rgb normalize

--- demo.py
+++ demo.py
@@ -11,11 +11,12 @@ img_gray = cv2.cvtColor(ll_img, cv2.COLOR_BGR2GRAY)
 img_gray_bilateral = cv2.bilateralFilter(img_gray, 5, 21, 21)
 img_gray_bilateral[img_gray_bilateral==0]=1
 img_d = img_gray/img_gray_bilateral
+# img_eq_bg = cv2.equalizeHist(img_gray_bilateral)
 img_eq_bg = cv2.createCLAHE().apply(img_gray_bilateral)
 
 shift = img_eq_bg.astype(np.int8) - img_gray_bilateral.astype(np.int8)
 scale = shift/img_gray_bilateral
-scale[img_gray_bilateral<=0.] = 0.
+# scale[img_gray_bilateral<=0.] = 0.
 
 alpha = (1-u*shift)
 
@@ -34,15 +35,16 @@ img_min = cv2.merge((img_min,img_min,img_min))
 # img_min[img_min<0] = 0
 
 img_rgb_nl = cv2.merge((B_, G_, R_))
+
+# RGB normalize
 img_rgb_nl[img_rgb_nl>255] = 255
 img_rgb_nl[img_rgb_nl<0] = 0
+# img_rgb_nl[img_rgb_nl>255.] = (255./img_max * img_rgb_nl)[img_rgb_nl>255.]
+# img_rgb_nl[img_rgb_nl<0.] = (img_max/(img_max-img_min)*(img_rgb_nl - img_min))[img_rgb_nl<0.]
 
-img_rgb_nl[img_rgb_nl>255.] = (255./img_max * img_rgb_nl)[img_rgb_nl>255.]
-img_rgb_nl[img_rgb_nl<0.] = (img_max/(img_max-img_min)*(img_rgb_nl - img_min))[img_rgb_nl<0.]
 
-
-img_ycbcr = cv2.cvtColor(img_rgb_nl.astype(np.uint8), cv2.COLOR_BGR2YCR_CB)
-(Y, CR, CB) = cv2.split(img_ycbcr)
+img_ycrcb = cv2.cvtColor(img_rgb_nl.astype(np.uint8), cv2.COLOR_BGR2YCR_CB)
+(Y, CR, CB) = cv2.split(img_ycrcb)
 
 output = (img_d - 1) * img_gray
 output = cv2.merge((output, output, output)) + img_rgb_nl
@@ -52,10 +54,18 @@ output = output.astype(np.uint8)
 
 
 cv2.imshow('Low-Light Image', ll_img)
-cv2.imshow('F_gray_bilateral', img_gray_bilateral)
-cv2.imshow('F_d', img_d)
-cv2.imshow('scale', scale)
+# cv2.imshow('F_gray_bilateral', img_gray_bilateral)
+# cv2.imshow('F_d', img_d)
+# cv2.imshow('shift', shift)
+# cv2.imshow('scale', scale)
 cv2.imshow('output', output)
+
+
+img_lab = cv2.cvtColor(ll_img, cv2.COLOR_BGR2LAB)
+(L, A, B) = cv2.split(img_lab)
+L = cv2.createCLAHE(clipLimit=2.0,).apply(L)
+clahe = cv2.cvtColor(cv2.merge((L, A, B)), cv2.COLOR_LAB2BGR)
+cv2.imshow('clahe', clahe)
 
 cv2.imwrite('output.jpg', output)
 
